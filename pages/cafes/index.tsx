03c7620d94a1b8fe94afb5c88627b5843cdaf6db
Rafael Rocha de azevedo <rafaelrochadeazevedo@Mac-mini-de-Rafael.local>
2022-03-05 20:18:53 -0300

    refactor: adding main into the layout component

--- pages/cafes/index.tsx
+++ pages/cafes/index.tsx
@@ -64,43 +64,40 @@ export default function Coffes() {
           content="Ow! café ? Restaurant. ATENDEMOS: (SEG- TERÇA 11H-18H), (QUARTA A SEXTA 9H-18H) e (SÁBADOS 9H-16H.). "
         />
       </Head>
+      <section className="lg:h-96 bg-zinc-900 flex justify-center items-center">
+        <h1 className="text-4xl tracking-tight font-semibold text-white sm:text-5xl md:text-6xl text-center mt-10">
+          Cafés
+        </h1>
+      </section>
+      <section className="-mt-20">
+        <div className="">
+          <div className="max-w-2xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <h2 className="sr-only">Products</h2>
 
-      <main>
-        <section className="lg:h-96 bg-zinc-900 flex justify-center items-center">
-          <h1 className="text-4xl tracking-tight font-semibold text-white sm:text-5xl md:text-6xl text-center mt-10">
-            Cafés
-          </h1>
-        </section>
-        <section className="-mt-20">
-          <div className="">
-            <div className="max-w-2xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
-              <h2 className="sr-only">Products</h2>
-
-              <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                {products.map((product) => (
-                  <Link href={"/cafes/" + product.id}>
-                    <a key={product.id} href={product.href} className="group">
-                      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                        <img
-                          src={product.imageSrc}
-                          alt={product.imageAlt}
-                          className="w-full h-full object-center object-cover group-hover:opacity-75"
-                        />
-                      </div>
-                      <h3 className="mt-4 text-sm text-gray-700">
-                        {product.name}
-                      </h3>
-                      <p className="mt-1 text-lg font-medium text-gray-900">
-                        {product.price}
-                      </p>
-                    </a>
-                  </Link>
-                ))}
-              </div>
+            <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+                <Link href={"/cafes/" + product.id}>
+                  <a key={product.id} href={product.href} className="group">
+                    <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                      <img
+                        src={product.imageSrc}
+                        alt={product.imageAlt}
+                        className="w-full h-full object-center object-cover group-hover:opacity-75"
+                      />
+                    </div>
+                    <h3 className="mt-4 text-sm text-gray-700">
+                      {product.name}
+                    </h3>
+                    <p className="mt-1 text-lg font-medium text-gray-900">
+                      {product.price}
+                    </p>
+                  </a>
+                </Link>
+              ))}
             </div>
           </div>
-        </section>
-      </main>
+        </div>
+      </section>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import { Header } from "../components/Layout/Header";
 
 export default function Home() {
   return (
@@ -12,44 +11,42 @@ export default function Home() {
           content="Ow! café ? Restaurant. ATENDEMOS: (SEG- TERÇA 11H-18H), (QUARTA A SEXTA 9H-18H) e (SÁBADOS 9H-16H.). "
         />
       </Head>
-      <main>
-        <section className="lg:h-screen bg-zinc-900">
-          <div className="container mx-auto px-2 sm:px-6 lg:px-8 sm:text-center lg:text-left">
-            {/* left side */}
-            <div className="pt-40  sm:px-6 lg:px-0 md:pt-40 lg:pt-40 pb-20 lg:pb-0 xl:pt-64 2xl:pt-80">
-              <h2 className="text-4xl tracking-tight font-semibold text-white sm:text-5xl md:text-6xl">
-                Café não é amargo
-              </h2>
-              <p className="mt-3 font-medium text-base text-zinc-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Redescubra o café, sabores e aromas exclusivos feitos com
-                dedicação e paixão por quem realmente entende.
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  <Link href="cafes">
-                    <a className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-zinc-900 bg-yellow-500 hover:bg-yellow-700 md:text-lg md:px-10">
-                      Cafés
-                    </a>
-                  </Link>
-                </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <Link href="gastronomia">
-                    <a
-                      href="#"
-                      className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-zinc-900 bg-yellow-100 hover:bg-indigo-200 md:text-lg md:px-10"
-                    >
-                      Gastronomia
-                    </a>
-                  </Link>
-                </div>
+      <section className="lg:h-screen bg-zinc-900">
+        <div className="container mx-auto px-2 sm:px-6 lg:px-8 sm:text-center lg:text-left">
+          {/* left side */}
+          <div className="pt-40  sm:px-6 lg:px-0 md:pt-40 lg:pt-40 pb-20 lg:pb-0 xl:pt-64 2xl:pt-80">
+            <h2 className="text-4xl tracking-tight font-semibold text-white sm:text-5xl md:text-6xl">
+              Café não é amargo
+            </h2>
+            <p className="mt-3 font-medium text-base text-zinc-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              Redescubra o café, sabores e aromas exclusivos feitos com
+              dedicação e paixão por quem realmente entende.
+            </p>
+            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div className="rounded-md shadow">
+                <Link href="cafes">
+                  <a className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-zinc-900 bg-yellow-500 hover:bg-yellow-700 md:text-lg md:px-10">
+                    Cafés
+                  </a>
+                </Link>
+              </div>
+              <div className="mt-3 sm:mt-0 sm:ml-3">
+                <Link href="gastronomia">
+                  <a
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-2 border border-transparent text-base font-medium rounded-md text-zinc-900 bg-yellow-100 hover:bg-indigo-200 md:text-lg md:px-10"
+                  >
+                    Gastronomia
+                  </a>
+                </Link>
               </div>
             </div>
-            {/* right side */}
-            <div></div>
           </div>
-        </section>
-        <section></section>
-      </main>
+          {/* right side */}
+          <div></div>
+        </div>
+      </section>
+      <section></section>
     </>
   );
 }
