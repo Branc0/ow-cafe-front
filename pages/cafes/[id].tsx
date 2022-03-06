ea1a96001873475a15fe30fdddffa22305a7d68d
Rafael Rocha de azevedo <rafaelrochadeazevedo@Mac-mini-de-Rafael.local>
2022-03-05 21:23:48 -0300

    WIP: creating coffes detail

--- pages/cafes/[id].tsx
+++ pages/cafes/[id].tsx
@@ -1,9 +1,13 @@
-const Details = () => {
-  return (
-    <div>
-      <h1>Details</h1>
-    </div>
-  );
+export const getStaticPaths = async () => {};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
 };
 
-export default Details;
+export default function Detail(context) {
+  return (
+    <div>
+      <h1>Details of {context.id}</h1>
+    </div>
+  );
+}
